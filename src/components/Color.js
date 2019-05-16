01c684dfe3a770d43cfebd88bfdd3bb253d37fea
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-16 10:43:44 -0700

    created color component to continue with the colors component

--- src/components/Color.js
+++ src/components/Color.js
@@ -6,5 +6,44 @@ const decimalFromHex = hex => {
 };
 
 const rgbFromHex = hex => {
-  return
+  return {
+    red: decimalFromHex(hex.slice(1, 3)),
+    green: decimalFromHex(hex.slice(3, 5)),
+    blue: decimalFromHex(hex.slice(5, 7))
+  };
+};
+
+function Color({ name, hex }) {
+  const rgb = rgbFromHex(hex);
+
+  const colorStyle = {
+    display: 'inline-block',
+    backgroundColor: hex,
+    width: '.8em',
+    height: '.8em'
+  };
+
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>{name || hex}<div style={colorStyle}></div></dd>
+
+      <dt>HEX</dt>
+      <dd>{hex}</dd>
+
+      <dt>RGB</dt>
+      <dd>
+        <p>RED-- {rgb.red}</p>
+        <p>GREEN-- {rgb.green}</p>
+        <p>BLUE-- {rgb.blue}</p>
+      </dd>
+    </dl>
+  );
 }
+
+Color.propTypes = {
+  name: PropTypes.string.isRequired,
+  hex: PropTypes.string.isRequired
+};
+
+export default Color;
--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Color from './Color';
 
 function Colors({ colors }) {
   const colorList = colors.map(color => {
     return (
       <li key={color}>
-        {colorList}
+        <Color name={color.name} hex={color.hex} />
       </li>
     );
   });
